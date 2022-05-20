251e2abef35738513898409b44a3f128c129a45c
meowth <37882842+shiba-sama@users.noreply.github.com>
2022-05-20 17:56:35 +0000

    Amadeus API wrapper (WIP) (#12)

--- server/api/amadeus.ts
+++ server/api/amadeus.ts
@@ -1,7 +1,7 @@
 // —————————————————————————————————————————————————————————————————————————————
 // Environment
 
-import { AMADEUS_KEY, AMADEUS_SECRET } from "../.env.ts"
+import { AMADEUS_KEY, AMADEUS_SECRET } from "../../.env.ts"
 
 const API = {
    test: "test.api.amadeus.com",
@@ -91,7 +91,7 @@ interface DestinationError {
  * @example
  * fetchDestinations("DEN", "GeneratedTokenHere")
  */
-function fetchDestinations(airport:string, token:string) {
+export function fetchDestinations(airport:string, token:string) {
    const URL = `https://${API.base}${API.destinations}?departureAirportCode=${airport}`
    const headers = {
       Authorization: `Bearer ${token}`,
@@ -99,3 +99,64 @@ function fetchDestinations(airport:string, token:string) {
    return fetch(URL, { headers })
       .then(res => res.json())
 }
+
+// —————————————————————————————————————————————————————————————————————————————
+// Amadeus Client
+
+class Client {
+   #key: string        // account id
+   #secret: string     // account secret
+   #token = ""         // app api token
+   static backoff = 1  // seconds
+   expiration = 0      // milliseconds
+
+   /**
+    * Requires Amadeus account `key` and `secret` to fetch an app token.
+    * https://developers.amadeus.com/self-service/apis-docs/guides/authorization-262
+    */
+   constructor(key:string, secret:string) {
+      this.#key    = key
+      this.#secret = secret
+   }
+
+   get isTokenExpired() {
+      return this.expiration < (Date.now() + Client.backoff)
+   }
+
+   async getToken() {
+      return await this.#useOrRefreshToken()
+   }
+
+   async #useOrRefreshToken() {
+      if (this.isTokenExpired) {
+         const response = await this.#newToken()
+         if ("error" in response) {
+            console.log("Amadeus token error: ", response)
+            throw Error(`Failure to obtain Amadeus token`)
+         }
+         this.#storeToken(response)
+      }
+      return this.#token
+   }
+
+   #newToken() {
+      const options = {
+         method: "POST",
+         body: new URLSearchParams({
+            grant_type: "client_credentials",
+            client_id: AMADEUS_KEY,
+            client_secret: AMADEUS_SECRET,
+         }),
+      }
+   
+      return fetch(API.token, options)
+         .then(r => r.json()) as Promise<TokenResponse | TokenError>
+   }
+
+   #storeToken(response:TokenResponse) {
+      this.#token = response["access_token"]
+      this.expiration = Date.now() + response["expires_in"] * 1000
+   }
+}
+
+export default Client
