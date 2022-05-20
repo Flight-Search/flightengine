5cc4da2267fba1235e954f223e251a64b2dafb18
meowth <37882842+shiba-sama@users.noreply.github.com>
2022-05-20 23:18:46 +0000

    Data cleanup & refactor (#13)

--- server/api/amadeus.ts
+++ server/api/amadeus.ts
@@ -1,6 +1,7 @@
 // —————————————————————————————————————————————————————————————————————————————
 // Environment
 
+import { TokenResponse, DestinationsResponse, Token } from "../../types/api.ts"
 import { AMADEUS_KEY, AMADEUS_SECRET } from "../../.env.ts"
 
 const API = {
@@ -13,25 +14,6 @@ const API = {
 // —————————————————————————————————————————————————————————————————————————————
 // Token Generation
 // https://developers.amadeus.com/self-service/apis-docs/guides/authorization-262
-
-type TokenResponse = {
-   type: "amadeusOAuth2Token"
-   username: string
-   application_name: string
-   client_id: string
-   token_type: "Bearer"
-   access_token: string
-   expires_in: number
-   state: "approved" | "expired"
-   scope: string
-}
-
-type TokenError = {
-   error: "invalid_client" | "invalid_grant" | "unauthorized_client"
-   error_description: string
-   code: number
-   title: string
-}
 
 /**
  * Obtains an access token which lasts for 29 minutes and 59 seconds.
@@ -50,48 +32,19 @@ export function getToken() {
    }
 
    return fetch(API.token, options)
-      .then(r => r.json()) as Promise<TokenResponse | TokenError>
+      .then(r => r.json()) as Promise<TokenResponse>
 }
 
 // —————————————————————————————————————————————————————————————————————————————
 // Direct Destinations
 // https://developers.amadeus.com/self-service/category/air/api-doc/airport-routes/api-reference
 
-interface DestinationResponse {
-   data: [
-      {
-         type     : string // location
-         subtype  : string // city
-         name     : string // Bangalore
-         iataCode : string // BLR
-      }[]
-   ],
-   meta: {
-      count : number // integer
-      sort  : string // iataCode
-      links : {
-         self : string // https://test.api.amadeus.com/v1/airport/direct-destination?departureAirportCode=NCE&max=2
-      }
-   },
-}
-
-interface DestinationError {
-   errors: [
-      {
-         "status": 400 | 500
-         "code": number
-         "title": string
-         "detail"?: string
-      }
-   ]
-}
-
 /**
  * Consumes `airport` code and bearer `token` and returns a list of destinations.
  * @example
  * fetchDestinations("DEN", "GeneratedTokenHere")
  */
-export function fetchDestinations(airport:string, token:string) {
+export function fetchDestinations(airport:string, token:string): Promise<DestinationsResponse> {
    const URL = `https://${API.base}${API.destinations}?departureAirportCode=${airport}`
    const headers = {
       Authorization: `Bearer ${token}`,
@@ -123,9 +76,10 @@ class Client {
       return this.expiration < (Date.now() + Client.backoff)
    }
 
-   async getToken() {
-      return await this.#useOrRefreshToken()
-   }
+   /**
+    * Returns promise of token.
+    */
+   getToken() { return this.#useOrRefreshToken() }
 
    async #useOrRefreshToken() {
       if (this.isTokenExpired) {
@@ -150,10 +104,10 @@ class Client {
       }
    
       return fetch(API.token, options)
-         .then(r => r.json()) as Promise<TokenResponse | TokenError>
+         .then(r => r.json()) as Promise<TokenResponse>
    }
 
-   #storeToken(response:TokenResponse) {
+   #storeToken(response:Token) {
       this.#token = response["access_token"]
       this.expiration = Date.now() + response["expires_in"] * 1000
    }
--- types/api.ts
+++ types/api.ts
@@ -23,5 +23,63 @@ export type Airport = {
 }
 
 export type AirportError = { error: { text: "No airport found" } }
-
 export type AirportResponse = Airport | AirportError
+
+// —————————————————————————————————————————————————————————————————————————————
+// https://developers.amadeus.com/self-service/apis-docs/guides/authorization-262
+
+export type Token = {
+   type: "amadeusOAuth2Token"
+   username: string
+   application_name: string
+   client_id: string
+   token_type: "Bearer"
+   access_token: string
+   expires_in: number
+   state: "approved" | "expired"
+   scope: string
+}
+
+export type TokenError = {
+   error: "invalid_client" | "invalid_grant" | "unauthorized_client"
+   error_description: string
+   code: number
+   title: string
+}
+
+export type TokenResponse = Token | TokenError
+
+// —————————————————————————————————————————————————————————————————————————————
+// Direct Destinations
+// https://developers.amadeus.com/self-service/category/air/api-doc/airport-routes/api-reference
+
+export interface Destinations {
+   data: [
+      {
+         type     : string // location
+         subtype  : string // city
+         name     : string // Bangalore
+         iataCode : string // BLR
+      }[]
+   ],
+   meta: {
+      count : number // integer
+      sort  : string // iataCode
+      links : {
+         self : string // https://test.api.amadeus.com/v1/airport/direct-destination?departureAirportCode=NCE&max=2
+      }
+   },
+}
+
+export interface DestinationsError {
+   errors: [
+      {
+         "status": 400 | 500
+         "code": number
+         "title": string
+         "detail"?: string
+      }
+   ]
+}
+
+export type DestinationsResponse = Destinations | DestinationsError
