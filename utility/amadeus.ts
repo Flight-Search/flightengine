dc4972732c46a47599a5cf76cc999a7a621216c2
meow <meow@>
2022-05-20 01:58:02 -0700

    Amadeus API for Destinations

--- utility/amadeus.ts
+++ utility/amadeus.ts
@@ -7,13 +7,51 @@ const API = {
    test: "test.api.amadeus.com",
    base: "test.api.amadeus.com/v1",
    destinations: "/airport/direct-destinations",
+   token: "https://test.api.amadeus.com/v1/security/oauth2/token",
 }
 
 // —————————————————————————————————————————————————————————————————————————————
 // Token Generation
 // https://developers.amadeus.com/self-service/apis-docs/guides/authorization-262
 
-const token = "https://test.api.amadeus.com/v1/security/oauth2/token"
+type TokenResponse = {
+   type: "amadeusOAuth2Token"
+   username: string
+   application_name: string
+   client_id: string
+   token_type: "Bearer"
+   access_token: string
+   expires_in: number
+   state: "approved" | "expired"
+   scope: string
+}
+
+type TokenError = {
+   error: "invalid_client" | "invalid_grant" | "unauthorized_client"
+   error_description: string
+   code: number
+   title: string
+}
+
+/**
+ * Obtains an access token which lasts for 29 minutes and 59 seconds.
+ * @example
+ * getToken()
+ *    .then(token => console.log(token))
+ */
+export function getToken() {
+   const options = {
+      method: "POST",
+      body: new URLSearchParams({
+         grant_type: "client_credentials",
+         client_id: AMADEUS_KEY,
+         client_secret: AMADEUS_SECRET,
+      }),
+   }
+
+   return fetch(API.token, options)
+      .then(r => r.json()) as Promise<TokenResponse | TokenError>
+}
 
 // —————————————————————————————————————————————————————————————————————————————
 // Direct Destinations
@@ -48,19 +86,16 @@ interface DestinationError {
    ]
 }
 
-const options = {
-   method: "POST",
-   headers: { "Content-Type": "application/x-www-form-urlencoded", },
-   body: `&grant_type=client_credentials` 
-      + `client_id=${AMADEUS_KEY}`
-      + `&client_secret=${AMADEUS_SECRET}`,
-}
-
 /**
- * Consumes `airport` code such as `"DEN"` and fetches a list of destinations.
+ * Consumes `airport` code and bearer `token` and returns a list of destinations.
+ * @example
+ * fetchDestinations("DEN", "GeneratedTokenHere")
  */
-function fetchDestinations(airport:string) {
+function fetchDestinations(airport:string, token:string) {
    const URL = `https://${API.base}${API.destinations}?departureAirportCode=${airport}`
-   return fetch(URL)
+   const headers = {
+      Authorization: `Bearer ${token}`,
+   }
+   return fetch(URL, { headers })
       .then(res => res.json())
 }
