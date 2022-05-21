68a4eacdb65f67932bfade98abb43b681ba40054
meow <meow@>
2022-05-20 17:28:42 -0700

    Data cleaning is bitter!

--- server/data/cleanupCountry.ts
+++ server/data/cleanupCountry.ts
@@ -15,8 +15,8 @@ type Airport = {
    iata: string
    name: string
    country: string
-   latitude: number
-   longitude: number
+   latitude: string
+   longitude: string
 }
 
 const INPUT = "./archive/airports.tsv"
@@ -27,26 +27,30 @@ const line_end = "\n"
 const comma = ","
 const quoted_word = /"(.*?)"/
 const parens_word = /\(.*?\)/
+const spaces = / +/g
+const space = " "
 
 // —————————————————————————————————————————————————————————————————————————————
 // Cleanup Data
 
-// header: [id, iata, name, location, latitude, longitude]
-const TSV = await Deno.readTextFile(INPUT)
-const [header, ...rows] = TSV.split(line_end)
-
-const airports: Airport[] = rows
-   .map(row => row.split(tab))
-   .map(row => ({
-      id: Number(row[0]),
-      iata: row[1],
-      name: cleanName(row[2]),
-      country: cleanCountry(row[3].split(comma).at(-1)?.trim() as string),
-      latitude: Number(row[4]),
-      longitude: Number(row[5]),
-   }))
-
 function cleanCountry(word:string) {
+   return word
+      .replace(/^[0-9] /, "")
+      .replace("Territoryof", "Territory of")
+      .replace("British Overseas Territory of", "British")
+      .replace(spaces, space)
+      .trim()
+}
+
+function cleanName(name:string) {
+   return name
+      .replace(quoted_word, "")
+      .replace(parens_word, "")
+      .replace(spaces, space)
+      .trim()
+}
+
+function mapCountry(word:string) {
    switch (word) {
       case "Côte d'Ivoire": 
          return "Ivory Coast"
@@ -58,11 +62,20 @@ function cleanCountry(word:string) {
    }
 }
 
-function cleanName(name:string) {
-   return name
-      .replace(quoted_word, "")
-      .replace(parens_word, "")
-}
+// header: [id, iata, name, location, latitude, longitude]
+const TSV = await Deno.readTextFile(INPUT)
+const [header, ...rows] = TSV.split(line_end)
+
+const airports: Airport[] = rows
+   .map(row => row.split(tab))
+   .map(row => ({
+      id: Number(row[0]),
+      iata: row[1],
+      name: cleanName(row[2]),
+      country: mapCountry(cleanCountry(row[3].split(comma).at(-1)?.trim() as string)),
+      latitude: row[4],
+      longitude: row[5],
+   }))
 
 // —————————————————————————————————————————————————————————————————————————————
 // Execute
