430042c7bb6921172f2c71a1b0df2240ee8ead4c
meow <meow@>
2022-05-23 12:28:43 -0700

    update

--- server/data/destinations/db_client.ts
+++ server/data/destinations/db_client.ts
@@ -4,14 +4,14 @@ import denver from "./denver.json" assert { type: "json" }
 const db = new DB("fly.db")
 const destinations = denver.data
 
-const query = db.prepareQuery()
+const addRoute = db.prepareQuery<[string, string]>(`
+   INSERT INTO Routes (from_iata, to_iata)
+      VALUES (:from, :to)
+`)
 
 for (const destination of destinations) {
-
+   addRoute.execute({
+      from: "DEN",
+      to: destination.iataCode,
+   })
 }
-
-// const data = db.query(`
-//    SELECT iata FROM Airports
-// `)
-
-db.close()
