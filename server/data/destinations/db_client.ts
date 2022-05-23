8f7cb86432e36bf6e43a1d51da00e5df0b6d4cef
meow <meow@>
2022-05-23 16:43:19 -0700

    update

--- server/data/destinations/db_client.ts
+++ server/data/destinations/db_client.ts
@@ -4,10 +4,17 @@ import denver from "./denver.json" assert { type: "json" }
 const db = new DB("fly.db")
 const destinations = denver.data
 
+// —————————————————————————————————————————————————————————————————————————————
+// Prepare Query
+
 const addRoute = db.prepareQuery<[string, string]>(`
    INSERT INTO Routes (from_iata, to_iata)
       VALUES (:from, :to)
+      ON CONFLICT(from_iata, to_iata) DO NOTHING
 `)
+
+// —————————————————————————————————————————————————————————————————————————————
+// Execute Query
 
 for (const destination of destinations) {
    addRoute.execute({
