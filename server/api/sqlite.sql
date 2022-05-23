b4dc936f287803a2f9fa32ff001d4a665dcc6219
meow <meow@>
2022-05-22 19:11:14 -0700

    update

--- server/api/sqlite.sql
+++ server/api/sqlite.sql
@@ -2,31 +2,31 @@
 -- Edges
 
 CREATE TABLE IF NOT EXISTS [Routes] (
-   [id]         INTEGER PRIMARY KEY,  -- route id
-   [from_iata]  VARCHAR(3),           -- iata origin
-   [from_name]  VARCHAR(3),           -- name origin
-   [to_iata]    VARCHAR(100),         -- iata destination
-   [to_name]    VARCHAR(100),         -- name destination
+   [id]         INTEGER PRIMARY KEY,   -- route id
+   [from_iata]  VARCHAR(3) NOT NULL,   -- origin iata
+   [from_name]  VARCHAR(3),            -- origin name
+   [to_iata]    VARCHAR(100) NOT NULL, -- destination iata
+   [to_name]    VARCHAR(100)           -- destination name
 );
 
-CREATE INDEX IF NOT EXISTS idx_from_to ON [Routes] (from, to);
-CREATE INDEX IF NOT EXISTS idx_to      ON [Routes] (to);
+CREATE INDEX IF NOT EXISTS idx_from_to ON [Routes] (from_iata, to_iata);
+CREATE INDEX IF NOT EXISTS idx_to      ON [Routes] (to_iata);
 
 -- —————————————————————————————————————————————————————————————————————————————
 -- Vertices
 
 CREATE TABLE IF NOT EXISTS [Airports] (
    [iata]  CHAR(3) PRIMARY KEY,  -- iata
-   [name]  VARCHAR(100),         -- name
+   [name]  VARCHAR(100),         -- name of airport
    [lat]   REAL,                 -- latitude
    [lon]   REAL,                 -- longitude
-   [tz]    INTEGER,              -- timezone
+   [tz]    INTEGER               -- timezone
 );
 
 -- —————————————————————————————————————————————————————————————————————————————
 -- Airlines
 
 CREATE TABLE IF NOT EXISTS [Airlines] (
-   [iata] CHAR(2) PRIMARY KEY,  -- iata
-   [name] VARCHAR(100),         -- Airline Company Name
+   [iata]  CHAR(2) PRIMARY KEY,  -- iata
+   [name]  VARCHAR(100)          -- name of airline company
 );
