539214c446a693c5498cf98b279ce0eb8fb2a362
meowth <37882842+shiba-sama@users.noreply.github.com>
2022-05-23 20:41:34 -0700

    Routes WIP (#18)

--- server/data/destinations/db_client.ts
+++ server/data/destinations/db_client.ts
@@ -1,17 +1,52 @@
-import { DB } from "https://deno.land/x/sqlite/mod.ts"
-import denver from "./denver.json" assert { type: "json" }
+import { DB } from "https://deno.land/x/sqlite/mod.ts";
 
-const db = new DB("fly.db")
-const destinations = denver.data
+const db = new DB("fly.db");
+const lines = Deno.readTextFileSync("./destinations.txt");
 
-const query = db.prepareQuery()
+// —————————————————————————————————————————————————————————————————————————————
+// Prepare Query
 
-for (const destination of destinations) {
-
-}
-
-// const data = db.query(`
-//    SELECT iata FROM Airports
+// const addRoute = db.prepareQuery<[string, string]>(`
+//    INSERT INTO Routes (from_iata, to_iata)
+//       VALUES (:from, :to)
+//       ON CONFLICT(from_iata, to_iata) DO NOTHING
 // `)
 
-db.close()
+// —————————————————————————————————————————————————————————————————————————————
+// Execute Query
+
+function json_to_route(json: string) {
+   const response = JSON.parse(json)
+   const origin = response.meta.links.self.slice(-3)
+}
+
+// for (const destination of destinations) {
+//    addRoute.execute({
+//       from: "DEN",
+//       to: destination.iataCode,
+//    })
+// }
+
+const data = lines
+  .split("\n")
+  .slice(0, -1)
+  .map((json) => JSON.parse(json))
+  .filter((obj) => !obj.warnings);
+
+const errorExample = {
+   warnings: [
+      {
+      code: 0,
+      title: "Data Not Found",
+      detail: "No destination is available",
+      source: { parameter: "departureAirportCode" },
+      status: 200,
+      },
+   ],
+   meta: {
+      count: 0,
+      links: {
+      self: "https://test.api.amadeus.com/v1/airport/direct-destinations?departureAirportCode=AAM",
+      },
+   },
+}
--- server/data/destinations/destinations.ts
+++ server/data/destinations/destinations.ts
@@ -1,3 +1,63 @@
+<<<<<<< HEAD
+import Client, { fetchDestinations } from "../../api/amadeus.ts"
+import { AMADEUS_KEY, AMADEUS_SECRET } from "../../../.env.ts"
+import {
+   Destinations,
+   DestinationsError,
+   DestinationsResponse,
+} from "../../../types/api.ts"
+
+import airports from "../airports.json" assert { type: "json" }
+
+// —————————————————————————————————————————————————————————————————————————————
+// Type
+
+type Airport = {
+   id: number
+   iata: string
+   name: string
+   country: string
+   latitude: number
+   longitude: number
+}
+
+// —————————————————————————————————————————————————————————————————————————————
+// Environment
+
+const client = new Client(AMADEUS_KEY, AMADEUS_SECRET)
+await client.getToken()
+
+function * naturals(max=5) {
+   for (let i=0; i<max; i++) yield i
+}
+
+const OUTPUT_FILE = "destinations.txt"
+
+// —————————————————————————————————————————————————————————————————————————————
+// Execute
+
+// for (const n of naturals(5)) {
+//    let routes = await fetchDestinations(airports[n].iata, await client.getToken())
+//    Deno.writeTextFileSync(
+//       "destinations.txt",
+//       JSON.stringify(routes) + "\n",
+//       { append: true }
+//    )
+// }
+
+fetchDestinations("DEN", await client.getToken())
+   .then(destinations => {
+      Deno.writeTextFileSync(
+         OUTPUT_FILE,
+         JSON.stringify(destinations) + "\n",
+         { append: true }
+      )
+   })
+
+// —————————————————————————————————————————————————————————————————————————————
+// Example
+||||||| 255ccff
+=======
 import Client, { fetchDestinations } from "../../api/amadeus.ts"
 import { AMADEUS_KEY, AMADEUS_SECRET } from "../../../.env.ts"
 import {
@@ -93,3 +153,4 @@ const error = {
       }
    }
 }
+>>>>>>> main
