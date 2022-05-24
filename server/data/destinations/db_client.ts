f631f876265a39154ee5ef042e44714f356a04d8
meow <meow@>
2022-05-24 11:21:36 -0700

    update

--- server/data/destinations/db_client.ts
+++ server/data/destinations/db_client.ts
@@ -4,30 +4,54 @@ const db = new DB("fly.db");
 const lines = Deno.readTextFileSync("./destinations.txt");
 
 // —————————————————————————————————————————————————————————————————————————————
+// Type
+
+type Destinations = {
+   data: {
+         type     : string // location
+         subtype  : string // city
+         name     : string // Bangalore
+         iataCode : string // BLR
+   }[],
+   meta: {
+      count : number // integer
+      sort  : string // iataCode
+      links : {
+         self : string // https://test.api.amadeus.com/v1/airport/direct-destination?departureAirportCode=NCE&max=2
+      }
+   },
+}
+
+// —————————————————————————————————————————————————————————————————————————————
 // Prepare Query
 
 const addRoute = db.prepareQuery<[string, string]>(`
    INSERT INTO Routes (from_iata, to_iata)
       VALUES (:from, :to)
-      ON CONFLICT (from_iata, to_iata) DO IGNORE
+      ON CONFLICT (from_iata, to_iata) DO NOTHING
+   ;
 `)
 
 // —————————————————————————————————————————————————————————————————————————————
 // Execute Query
 
-function json_to_route(json: string) {
-   const response = JSON.parse(json)
-   const from = response.meta.links.self.slice(-3)
-
-   addRoute.execute({
-      from,
-      to: response.data.iata
-   })
-}
+db.query("BEGIN TRANSACTION;")
 
 const data = lines
-  .split("\n")
-  .map((json) => JSON.parse(json))
-  .filter((obj) => !obj.warnings)
+   .split("\n")
+   .map((json) => JSON.parse(json))
+   .filter(obj => !obj.warnings)
+   .filter(obj => !obj.errors)
+   .forEach((obj:Destinations) => {
+      const from = obj.meta.links.self.slice(-3)
+      const targets = obj.data.map(d => d.iataCode)
+      for (const to of targets) {
+         try { addRoute.execute({ from, to }) }
+         catch(e) { console.log(e) }
+         console.log(`${from} -> ${to}`)
+      }
+   })
 
-console.log(data.length)
+db.query("END TRANSACTION;")
+
+console.log(data)
--- server/data/destinations/sqlite.sql
+++ server/data/destinations/sqlite.sql
@@ -6,13 +6,13 @@ CREATE TABLE IF NOT EXISTS [Routes] (
    [from_iata]  VARCHAR(3)   NOT NULL,    -- origin iata
    [from_name]  VARCHAR(3)   NULL,        -- origin name
    [to_iata]    VARCHAR(100) NOT NULL,    -- destination iata
-   [to_name]    VARCHAR(100) NULL         -- destination name
+   [to_name]    VARCHAR(100) NULL,        -- destination name
 
    FOREIGN KEY (from_iata) REFERENCES Airports(iata)
 );
 
-CREATE INDEX IF NOT EXISTS idx_from_to ON [Routes] (from_iata, to_iata);
-CREATE INDEX IF NOT EXISTS idx_to      ON [Routes] (to_iata);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_from_to ON [Routes] (from_iata, to_iata);
+CREATE        INDEX IF NOT EXISTS idx_to      ON [Routes] (to_iata);
 
 -- —————————————————————————————————————————————————————————————————————————————
 -- Vertices
