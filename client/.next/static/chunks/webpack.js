dd3086fece28c0908ee59f1af7987cfbddcb9962
garofalof <fgarofalo1493@gmail.com>
2022-06-02 23:03:13 -0700

    initial front end commit

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ec6331d05e3719c3"; }
+/******/ 		__webpack_require__.h = function() { return "a812cfc8ad01ddf1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
